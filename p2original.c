998fc56f96ccdd8a53faa3e698cacf340b72859c
M SANJEEV <98035905+sanjureddysj@users.noreply.github.com>
2022-03-02 09:57:24 +0000

    p1 debugged

--- p2original.c
+++ p2original.c
@@ -1,40 +1,42 @@
-#include<stdio,h>
+#include<stdio.h>
 
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
 {
-  printf("Enter the first sides of Triangle"\n);
+  printf("Enter the first sides of Triangle\n");
   scanf("%f %f", x1,y1);
 
-  printf("Enter the second sides of Triangle"\n);
+  printf("Enter the second sides of Triangle\n");
   scanf("%f %f", x2,y2);
 
-  printf("Enter the third sides of Triangle"\n);
+  printf("Enter the third sides of Triangle\n");
   scanf("%f %f", x3,y3);
 }
 
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3) 
 {
-  if((a+b>c)&&(b+c>a)&&(c+a>b))
-  {
-    printf("The Points forms Triangle", a,b,c);
-  }
-
+  int area;
+  area=1/2*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+  if(area==0)
+  return 0;
   else
-  {
-    printf("The Points doesn't forms a Triangle", a,b,c);
-  }
+  return 1;
 }
 
-void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
-{
-  printf("The Points forms the triangle", istriangle);
+void output(int a)
+{  
+    if(a==0)
+    printf("Triangle doesn't form");
+    else
+    printf("Triangle forms");
 }
 
 int main()
 {
-  float x1,y1,x2,y2,x3,y3,istriangle;
-  input(&x1,&y1,&x2,&y2,&x3,&y3);
-  is_tiangle(x1,y1,x2,y2,x3,y3);
-  output(x1,y1,x2,y2,x3,y3,istraingle);
+  int a;
+  float x1,y1,x2,y2,x3,y3;
+  input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+  a=is_triangle(x1,y1,x2,y2,x3,y3);
+  output(a);
+  return 0;
 }
 
